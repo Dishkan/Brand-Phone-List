186cacb9c8c7feb82a927a396593f3b8f0aa1982
Dilshod Khudoyarov <DH.17@mail.ru>
2020-12-13 18:49:09 +0500

    Update search.blade.php

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -2,7 +2,7 @@
 
 @section('search')
 <div class="container">
-    @if(isset($details))
+    @if(isset($q))
         <p> The search results for your query <b> {{ $query }} </b> are :</p>
     <table class="table table-striped">
         <tbody>
@@ -39,6 +39,26 @@
             @endforeach
         </tbody>
     </table>
+	@else
+	<div class="container">
+        <p> The search results for your query <b> {{ $query }} </b> are :</p>
+    <table class="table table-striped">
+        <tbody>
+            <tr>
+                <td>Nothing was found!</td>
+			</tr>
+			<tr>
+			  <td>
+                <a href="/">
+                    <button class="btn btn-info">
+                        <i class="fa fa-btn fa-create"></i> Back
+                    </button>
+				</a>
+			  </td>
+			</tr>
+        </tbody>
+    </table>  
+</div>
     @endif
 </div>
 @endsection

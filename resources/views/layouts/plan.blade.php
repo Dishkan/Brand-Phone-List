fccfd4d51b61ae5a596a85996fffadb4fb9bd196
Dilshod Khudoyarov <DH.17@mail.ru>
2020-12-13 18:56:09 +0500

    Update plan.blade.php

--- resources/views/layouts/plan.blade.php
+++ resources/views/layouts/plan.blade.php
@@ -27,6 +27,8 @@
     @yield('content')
 	
     @yield('search')
+	
+    @yield('search_notfound')
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
